c7cb34b6355d560521928e37e9b639a807c4f613
Alexander2116 <Aleksander2116k@gmail.com>
2023-02-17 23:53:54 +0000

    a

--- MPhys/GUI/NDFForm.Designer.cs
+++ MPhys/GUI/NDFForm.Designer.cs
@@ -35,6 +35,10 @@ namespace MPhys.GUI
             this.NDF1COM = new System.Windows.Forms.TextBox();
             this.NDF2COM = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonSet1 = new System.Windows.Forms.Button();
+            this.buttonSet2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -64,7 +68,7 @@ namespace MPhys.GUI
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(137, 92);
+            this.label3.Location = new System.Drawing.Point(52, 92);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(34, 13);
             this.label3.TabIndex = 2;
@@ -72,16 +76,18 @@ namespace MPhys.GUI
             // 
             // NDF1COM
             // 
-            this.NDF1COM.Location = new System.Drawing.Point(177, 89);
+            this.NDF1COM.Location = new System.Drawing.Point(92, 89);
             this.NDF1COM.Name = "NDF1COM";
-            this.NDF1COM.Size = new System.Drawing.Size(98, 20);
+            this.NDF1COM.ReadOnly = true;
+            this.NDF1COM.Size = new System.Drawing.Size(37, 20);
             this.NDF1COM.TabIndex = 3;
             // 
             // NDF2COM
             // 
-            this.NDF2COM.Location = new System.Drawing.Point(495, 89);
+            this.NDF2COM.Location = new System.Drawing.Point(413, 89);
             this.NDF2COM.Name = "NDF2COM";
-            this.NDF2COM.Size = new System.Drawing.Size(98, 20);
+            this.NDF2COM.ReadOnly = true;
+            this.NDF2COM.Size = new System.Drawing.Size(37, 20);
             this.NDF2COM.TabIndex = 5;
             // 
             // label4
@@ -89,11 +95,47 @@ namespace MPhys.GUI
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.White;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(455, 92);
+            this.label4.Location = new System.Drawing.Point(373, 92);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "COM";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(135, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(54, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // buttonSet1
+            // 
+            this.buttonSet1.Location = new System.Drawing.Point(195, 86);
+            this.buttonSet1.Name = "buttonSet1";
+            this.buttonSet1.Size = new System.Drawing.Size(75, 23);
+            this.buttonSet1.TabIndex = 7;
+            this.buttonSet1.Text = "Set";
+            this.buttonSet1.UseVisualStyleBackColor = true;
+            this.buttonSet1.Click += new System.EventHandler(this.buttonSet1_Click);
+            // 
+            // buttonSet2
+            // 
+            this.buttonSet2.Location = new System.Drawing.Point(525, 86);
+            this.buttonSet2.Name = "buttonSet2";
+            this.buttonSet2.Size = new System.Drawing.Size(75, 23);
+            this.buttonSet2.TabIndex = 9;
+            this.buttonSet2.Text = "Set";
+            this.buttonSet2.UseVisualStyleBackColor = true;
+            this.buttonSet2.Click += new System.EventHandler(this.buttonSet2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(465, 88);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(54, 21);
+            this.comboBox2.TabIndex = 8;
             // 
             // NDFForm
             // 
@@ -101,6 +143,10 @@ namespace MPhys.GUI
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSet2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.buttonSet1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.NDF2COM);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.NDF1COM);
@@ -123,5 +169,9 @@ namespace MPhys.GUI
         private System.Windows.Forms.TextBox NDF1COM;
         private System.Windows.Forms.TextBox NDF2COM;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonSet1;
+        private System.Windows.Forms.Button buttonSet2;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
--- MPhys/GUI/NDFForm.cs
+++ MPhys/GUI/NDFForm.cs
@@ -8,23 +8,118 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
+using MPhys.Devices;
+using System.IO.Ports;
 
 namespace MPhys.GUI
 {
     public partial class NDFForm : Form
     {
-        public bool is_open;
+        private FC102C NDF1;
+        private FC102C NDF2;
 
         public NDFForm()
         {
             InitializeComponent();
-            is_open = true;
             this.Location = new Point(60, 26);
             this.ControlBox = false;
             this.TopLevel = false;
             this.TopMost = true;
-            NDF1COM.Text = ConfigurationManager.AppSettings.Get("NDF1");
-            NDF2COM.Text = ConfigurationManager.AppSettings.Get("NDF2");
+
+            modify_com_boxes();
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+                comboBox2.Items.Add(s);
+            }
+        }
+
+        #region Private functions 
+        private void check_com(string com1, string com2)
+        {
+            // NDF1
+            try
+            {
+                NDF1 = new FC102C(com1);
+                if (NDF1.IsOpen() == 1)
+                {
+                    NDF1COM.BackColor = Color.Green;
+                }
+                else
+                {
+                    NDF1COM.BackColor = Color.Red;
+                }
+            }
+            catch (Exception ex) { NDF1COM.BackColor = Color.Red; }
+
+            // NDF2
+            try
+            {
+                NDF2 = new FC102C(com2);
+                if (NDF2.IsOpen() == 1)
+                {
+                    NDF2COM.BackColor = Color.Green;
+                }
+                else
+                {
+                    NDF2COM.BackColor = Color.Red;
+                }
+            }
+            catch (Exception ex) { NDF2COM.BackColor = Color.Red; }
+        }
+
+        private void modify_com_boxes()
+        {
+            string NDF1port = ConfigurationManager.AppSettings.Get("NDF1");
+            string NDF2port = ConfigurationManager.AppSettings.Get("NDF2");
+            NDF1COM.Text = NDF1port;
+            NDF2COM.Text = NDF2port;
+
+            check_com(NDF1port, NDF2port);
+
+        }
+
+        static void UpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+        #endregion
+
+        private void buttonSet1_Click(object sender, EventArgs e)
+        {
+            string new_com = comboBox1.SelectedItem.ToString();
+            NDF1COM.Text = new_com;
+
+            UpdateAppSettings("NDF1", new_com);
+            modify_com_boxes();
+        }
+
+        private void buttonSet2_Click(object sender, EventArgs e)
+        {
+            string new_com = comboBox2.SelectedItem.ToString();
+            NDF2COM.Text = new_com;
+
+            UpdateAppSettings("NDF2", new_com);
+            modify_com_boxes();
         }
     }
 }
